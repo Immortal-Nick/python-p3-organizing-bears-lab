c45b253a76bc79c412f928b69eb13ed5ac8466ea
Nixon <kiplangatnixon28@gmail.com>
2024-09-20 15:40:39 +0300

    creates a bears database and updates it

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+id INTEGER [PRIMARY KEY],
+name TEXT,
+age integer,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive bolean
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "white", "calmn", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "white", "mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabby", 7, "F", "Black", "hostile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "Brown", "docile", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "white", "unknown", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 15, "M", "Grey", "Short Tempered", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "white", "Unpredictable", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "white", NULL, 1);
